ea0a11672501e2871079138bfb59ccc82d2bda74
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2017-05-07 17:34:23 -0300

    add fotter and site map

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,24 @@
 		<footer class="footer">
 			<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<h5 class="white-text">Vestindo certo</h5>
+                	<p>Selecionar a melhor roupa nunca foi tão fácil.</p>
+				</div>
+				<div class="col-sm-6">
+					<h5 class="white-text">Links</h5>
+	                <ul>
+	                  <li><a href="#!">Simulador</a></li>
+	                  <li><a href="#!">Apresentação</a></li>
+	                  <li><a href="#!">Perfil</a></li>
+	                  <li><a href="#!">Sobre</a></li>
+	                </ul>
+				</div>
+			</div>
 				<div class="row">
 					<div class="col-sm-12">
 						<p class="text-center">
-							Desenvolvido por Pentatec Corp.
+							2017 &copy; Todos os direitos reservados
 						</p>
 					</div>
 				</div>
--- include/head.php
+++ include/head.php
@@ -8,7 +8,7 @@
 		<script type="text/javascript" src="plugin/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="plugin/bootstrap-3.3.6-dist/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="plugin/font-awesome-4.6.3/css/font-awesome.min.css">
-		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/		css?family=Open+Sans:400,300italic,300,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<script type="text/javascript" src="js/main.js"></script>
