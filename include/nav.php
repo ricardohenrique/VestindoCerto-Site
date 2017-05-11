e07a5d349327732ad6affbc83c3d19bca52b5589
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2017-05-11 18:05:35 -0300

    add dropdown menu Institucional and Business

--- include/nav.php
+++ include/nav.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar navbar-default navbar-fixed-top">
 	<div class="container">
 		<div class="navbar-header">
@@ -12,20 +11,25 @@
 		</div>
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="quem-somos.php"><i class="fa fa-university" aria-hidden="true"></i>Quem Somos</a></li>
-				<li><a href="como-funciona.php">Como Funciona <i class="fa fa-question" aria-hidden="true"></i></a></li>
-				<li><a href="contato.php"><i class="fa fa-map-marker" aria-hidden="true"></i> Contato</a></li>
-				<li><a href="#!"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
 				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-						<i class="fa fa-caret-down" aria-hidden="true"></i>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Institucional <i class="fa fa-caret-down" aria-hidden="true"></i>
 					</a>
 					<ul class="dropdown-menu">
-						<li><a href="simulador.php"><i class="fa fa-shield" aria-hidden="true"></i> Simulador</a></li>
-						<li><a href="apresentacao.php"><i class="fa fa-rocket" aria-hidden="true"></i> Apresentação</a></li>
-						<li><a href="usuario/index.php"><i class="fa fa-user" aria-hidden="true"></i> Usuários</a></li>
-					</ul>
+						<li><a href="quem-somos.php"><i class="fa fa-university" aria-hidden="true"></i> Sobre Nós</a></li>
+						<li><a href="contato.php"><i class="fa fa-map-marker" aria-hidden="true"></i> Contato</a></li>
+					</ul>					
 		        </li>
+		        <li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+					Business <i class="fa fa-caret-down" aria-hidden="true"></i>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="como-funciona.php">Como Funciona <i class="fa fa-question" aria-hidden="true"></i></a></li>
+						<li><a href="simulador.php"><i class="fa fa-shield" aria-hidden="true"></i> Simulador</a></li>
+						<li><a href="apresentacao.php"><i class="fa fa-rocket" aria-hidden="true"></i> Apresentação</a></li>			
+					</ul>				
+		        </li>								
+				<li><a href="#!"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>				
 			</ul>
 		<form class="navbar-form navbar-left">
 	        <div class="form-group">
