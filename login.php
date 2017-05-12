cde742d97028e7518c79934b6cabcc0d5a555275
JhonnyRohr <jhonny.rohr@gmail.com>
2017-05-11 22:40:50 -0300

    css login

--- login.php
+++ login.php
@@ -3,19 +3,32 @@
     <div class="container-fluid contato" id="login">
         <div class="container">
     		<div class="row">
-    			<div class="col-sm-offset-2 col-sm-8">
+    			<div class="col-sm-offset-4 col-sm-4">
     				<h3>Login</h3>
                     <form action="" method="POST" class="row">
-                        <div class="form-group col-sm-6">
-                            <input type="text" name="email" class="form-control" placeholder="E-mail: ">
-                        </div>
-                    </form>
-                    <form action="" method="POST" class="row">
-                        <div class="form-group col-sm-6">
-                            <input type="password" name="senha" class="form-control" placeholder="Senha: ">
-                        </div>
                         <div class="form-group col-sm-12">
-                            <button class="btn btn-default"> Entrar </button>
+                            <div class="input-group">
+                                <div class="input-group-addon form"><i class="fa fa-user" aria-hidden="true"></i></div>
+                                <input type="text" class="form-control" id="exampleInputAmount" placeholder="E-mail">
+                            </div>
+                        </div>
+
+                        <div class="form-group col-sm-12">
+                            <div class="input-group">
+                                <div class="input-group-addon"><i class="fa fa-key" aria-hidden="true"></i></div>
+                                <input type="password" name="senha" class="form-control" placeholder="Senha: ">
+                            </div>
+                        </div>  
+                            <div class="checkbox">
+                                <label class="col-sm-offset-1">
+                                    <input type="checkbox"> Remember me
+                                </label>
+                            </div>
+                        <div class="form-group col-sm-6">
+                            <button class="btn btn-default"><i class="fa fa-sign-in" aria-hidden="true"></i> Entrar </button>
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <button class="btn btn-default"><i class="fa fa-user-plus" aria-hidden="true"></i> Cadastre-se </button>
                         </div>
                     </form>
     			</div>
