e96d12f4dd2738c7de444750d33dbd050adad410
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2017-05-11 22:31:20 -0300

    fix footer

--- include/footer.php
+++ include/footer.php
@@ -1,28 +1,37 @@
-		<footer class="footer">
-			<div class="container">
-			<div class="row">
-				<div class="col-sm-6">
-					<h5 class="white-text">Vestindo certo</h5>
-                	<p>Selecionar a melhor roupa nunca foi tão fácil.</p>
-				</div>
-				<div class="col-sm-6">
-					<h5 class="white-text">Links</h5>
-	                <ul>
-	                  <li><a href="#!">Simulador</a></li>
-	                  <li><a href="#!">Apresentação</a></li>
-	                  <li><a href="#!">Perfil</a></li>
-	                  <li><a href="#!">Sobre</a></li>
-	                </ul>
-				</div>
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 footer-links">
+				<ul>
+	            	<li><a href="simulador.php">Simulador</a></li>
+	              	<li><a href="apresentacao.php">Apresentação</a></li>	         
+	              	<li><a href="quem-somos.php">Sobre Nós</a></li>
+	            </ul>								
 			</div>
-				<div class="row">
-					<div class="col-sm-12">
-						<p class="text-center">
-							2017 &copy; Todos os direitos reservados
-						</p>
-					</div>
-				</div>
+			<div class="col-sm-4 footer-links">
+				<ul>
+	            	<li><a href="simulador.php">Simulador</a></li>
+	              	<li><a href="apresentacao.php">Apresentação</a></li>	         
+	              	<li><a href="quem-somos.php">Sobre Nós</a></li>
+	            </ul>
 			</div>
-		</footer>
-	</body>
-</html>	
+			<div class="col-sm-4 footer-links">
+				<ul>
+	            	<li><a href="simulador.php">Simulador</a></li>
+	              	<li><a href="apresentacao.php">Apresentação</a></li>	         
+	              	<li><a href="quem-somos.php">Sobre Nós</a></li>
+	            </ul>
+			</div>			
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12 footer-copyright">	            
+				<div class="text-center">
+					<p>&copy; Copyright 2015-2017 Vestindo Certo corp.</p>
+				</div>				
+			</div>		
+		</div>
+	</div>	
+</footer>
