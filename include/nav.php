0d3dd9ffced5217bdc067f122bffd552ac7354b6
ricardohenrique <ricardo.hb@outlook.com>
2017-05-11 23:01:37 -0300

    add icons to nav

--- include/nav.php
+++ include/nav.php
@@ -12,24 +12,24 @@
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
 				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Institucional <i class="fa fa-caret-down" aria-hidden="true"></i>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-university" aria-hidden="true"></i> Institucional <i class="fa fa-caret-down" aria-hidden="true"></i>
 					</a>
 					<ul class="dropdown-menu">
-						<li><a href="quem-somos.php"><i class="fa fa-university" aria-hidden="true"></i> Sobre Nós</a></li>
-						<li><a href="contato.php"><i class="fa fa-map-marker" aria-hidden="true"></i> Contato</a></li>
-					</ul>					
+						<li><a href="quem-somos.php">Sobre Nós</a></li>
+						<li><a href="contato.php">Contato</a></li>
+					</ul>
 		        </li>
 		        <li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-					Business <i class="fa fa-caret-down" aria-hidden="true"></i>
+					<i class="fa fa-rocket" aria-hidden="true"></i> Business <i class="fa fa-caret-down" aria-hidden="true"></i>
 					</a>
 					<ul class="dropdown-menu">
-						<li><a href="como-funciona.php">Como Funciona <i class="fa fa-question" aria-hidden="true"></i></a></li>
-						<li><a href="simulador.php"><i class="fa fa-shield" aria-hidden="true"></i> Simulador</a></li>
-						<li><a href="apresentacao.php"><i class="fa fa-rocket" aria-hidden="true"></i> Apresentação</a></li>			
-					</ul>				
-		        </li>								
-				<li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>				
+						<li><a href="como-funciona.php">Como Funciona</a></li>
+						<li><a href="simulador.php">Simulador</a></li>
+						<li><a href="apresentacao.php">Apresentação</a></li>
+					</ul>
+		        </li>
+				<li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
 			</ul>
 		<form class="navbar-form navbar-left">
 	        <div class="form-group">
