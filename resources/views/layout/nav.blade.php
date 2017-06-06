a2a82036689aae22520f29b23e3928a060973afa
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2017-06-06 20:12:06 -0300

    add icon when user has conected

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -38,15 +38,15 @@
 					@if (!Auth::guest())
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+							<i class="fa fa-user" aria-hidden="true"></i>								
 								{{ Auth::user()->name }} <span class="caret"></span>
-							</a>
-
+							</a>							
 							<ul class="dropdown-menu" role="menu">
 								<li>
 									<a href="{{ url('/logout') }}"
 										onclick="event.preventDefault();
 													document.getElementById('logout-form').submit();">
-										Logout
+										Sair
 									</a>
 									<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
 										{{ csrf_field() }}
